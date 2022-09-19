53d1a4ad08f1d041cb3dfa925e9d75d40e135033
hobbiathan <hobbiathan@gmail.com>
2022-09-18 20:35:07 -0600

    update index func

--- chitchat/main.go
+++ chitchat/main.go
@@ -6,17 +6,28 @@ import (
 
 // Handler functions take a ResponseWriter as a param, and a pointer to a request as a the second
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
+	threads, err := data.Threads(); if err == nil {
+		_, err := session(w, r)
 
-	
-	templates := template.Must(template.ParaseFiles(files...))
+		public_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html"
+		}
 
-	threads, err := data.Threads() p if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
+		private_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html",
+		}
+
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		}
+		templates.ExecuteTEmplate(w, "layout", threads)
 	}
 }
 
